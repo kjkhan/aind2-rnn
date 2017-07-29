0d1b4e80db0755450559849b2c8c513fceae820b
Kahlil Khan <kjkhan@users.noreply.github.com>
2017-07-29 11:26:53 -0700

    draft completion of my_answers

--- my_answers.py
+++ my_answers.py
@@ -9,13 +9,21 @@ import keras
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
-    # containers for input/output pairs
+
+    # initialize arrays
     X = []
     y = []
-
+    
+    # split "series" array into window_size chunks
+    # and create output array
+    for i in range(len(series) - window_size):
+        X.append(series[i : i + window_size])
+        y.append(series[i + window_size])
+          
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
+    print(X.shape)
     y = np.asarray(y)
     y.shape = (len(y),1)
 
@@ -23,13 +31,26 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
-
+    model = Sequential()
+    model.add(LSTM(32, input_shape=(window_size,1)))
+    model.add(Dense(1, activation='tanh'))
+    return model
+    
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
+    
+    # found in the set of characters in the Sherlock Homes text
+    remove = ['"', '$', '%', '&', "'", '(', ')', '*', '-', '/', '@']
+    for i in remove:
+        text = text.replace(i, ' ')
+    
+    # replace non-English characters
+    text = text.replace('à', 'a')
+    text = text.replace('â', 'a')
+    text = text.replace('è', 'e')
+    text = text.replace('é', 'e')
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -38,9 +59,17 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    for i in range(0, len(text) - window_size, step_size):
+        inputs.append(text[i : i + window_size])
+        outputs.append(text[i + window_size])
+        
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size,num_chars)))
+    model.add(Dense(num_chars, activation='softmax'))
+    return model
+    
