56e01f42b7912d5431033817c10133e00718325c
Kahlil Khan <kjkhan@users.noreply.github.com>
2017-07-30 02:56:05 +0000

    fix regex for special characters

--- my_answers.py
+++ my_answers.py
@@ -31,26 +31,29 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
+    
+    # build 5-layer LSTM model with input size = window size, ouput to 1 value
+    # using hyperbolic tangent activation (so output is between -1 and 1)
     model = Sequential()
-    model.add(LSTM(32, input_shape=(window_size,1)))
+    model.add(LSTM(5, input_shape=(window_size,1)))
     model.add(Dense(1, activation='tanh'))
     return model
     
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    punctuation = ['!', ',', '.', ':', ';', '?']
+    import re
     
-    # found in the set of characters in the Sherlock Homes text
-    remove = ['"', '$', '%', '&', "'", '(', ')', '*', '-', '/', '@']
-    for i in remove:
-        text = text.replace(i, ' ')
+    punctuation = ['!', ',', '.', ':', ';', '?']
     
     # replace non-English characters
     text = text.replace('à', 'a')
     text = text.replace('â', 'a')
     text = text.replace('è', 'e')
     text = text.replace('é', 'e')
+    
+    # use regular expression to keep only a-z characters and punctuation listed above
+    text = re.sub('[^a-z!,.:;?]', ' ', text)
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -59,6 +62,7 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    # step through text and to create input arrays and output value
     for i in range(0, len(text) - window_size, step_size):
         inputs.append(text[i : i + window_size])
         outputs.append(text[i + window_size])
@@ -68,6 +72,9 @@ def window_transform_text(text, window_size, step_size):
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
+    
+    # build 200 layer LSTM model using 2D input arrary (window size, number of characters)
+    # since it is a classfication problem, output using softmax to get probability
     model = Sequential()
     model.add(LSTM(200, input_shape=(window_size,num_chars)))
     model.add(Dense(num_chars, activation='softmax'))
